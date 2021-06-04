be3d1d3386a121bb41cdff5d914207a4a84b2fb1
milovdpas <mpmpas1@avans.nl>
2021-06-04 16:41:49 +0200

    publish

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, url_for
+from flask import render_template
+from flask import request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        req = request.form
+        username = req.get("username")
+        return redirect(url_for('settings'))
+    nav = [
+        {"name": "Homepage", "url": url_for('login')},
+        {"name": "Statistieken", "url": '/'},
+        {"name": "Over ons", "url": '/'}
+        # {"name": "Statistieken", "url": url_for('statistics')},
+        # {"name": "Over ons", "url": url_for('about_us')},
+    ]
+    return render_template(
+        "login.html",
+        nav=nav
+    )
+
+@app.route('/settings')
+def settings():
+    return render_template(
+        "settings.html"
+    )
 
 if __name__ == '__main__':
     app.run()
