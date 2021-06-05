b038b8c004efb02b4501b4fd28562aede6dc4827
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-06-05 11:40:52 +0200

    update get navbar items

--- app.py
+++ app.py
@@ -4,28 +4,35 @@ from flask import request, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        req = request.form
-        username = req.get("username")
-        return redirect(url_for('settings'))
-    nav = [
+def getNavItems():
+    return [
         {"name": "Homepage", "url": url_for('login')},
         {"name": "Statistieken", "url": '/'},
         {"name": "Over ons", "url": '/'}
         # {"name": "Statistieken", "url": url_for('statistics')},
         # {"name": "Over ons", "url": url_for('about_us')},
     ]
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        req = request.form
+        username = req.get("username")
+        return redirect(url_for('settings'))
+
     return render_template(
         "login.html",
-        nav=nav
+        nav=getNavItems()
     )
 
 @app.route('/settings')
 def settings():
+    if request.method == "POST":
+        return redirect(url_for('settings'))
+    
     return render_template(
-        "settings.html"
+        "settings.html",
+        nav=getNavItems(),
     )
 
 if __name__ == '__main__':
